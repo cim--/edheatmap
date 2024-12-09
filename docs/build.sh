aa2213fb07c761366a383464b9203d82eee49865
Chris Morris <cim@oolite.org>
2024-12-09 21:05:28 +0000

    Some more ethos diagrams

--- docs/build.sh
+++ docs/build.sh
@@ -2,3 +2,7 @@
 
 dot ethos.gv -Tpng > ../public/images/ethos.png
 dot ethos2.gv -Tpng > ../public/images/ethos2.png
+dot acquisition.gv -Tpng > ../public/images/acquisition.png
+dot reinforcement.gv -Tpng > ../public/images/reinforcement.png
+dot undermining.gv -Tpng > ../public/images/undermining.png
+dot activities.gv -Tpng > ../public/images/activities.png
